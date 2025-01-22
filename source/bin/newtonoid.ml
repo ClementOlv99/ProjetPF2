f07baaada4c5a43a02063011a422610ea36a06fb
Tom <tom.gutierrez1040@gmail.com>
2025-01-22 15:42:49 +0100

    maj

--- source/bin/newtonoid.ml
+++ source/bin/newtonoid.ml
@@ -5,17 +5,6 @@ open Iterator
 (* exemple d'ouvertue d'un tel module de la bibliotheque : *)
 open Game
 
-module Init = struct
-  let dt = 1000. /. 60. (* 60 Hz *)
-end
-
-module Box = struct
-  let marge = 10.
-  let infx = 10.
-  let infy = 10.
-  let supx = 790.
-  let supy = 590.
-end
 
 let graphic_format =
   Format.sprintf
@@ -23,7 +12,8 @@ let graphic_format =
     (int_of_float ((2. *. Box.marge) +. Box.supx -. Box.infx))
     (int_of_float ((2. *. Box.marge) +. Box.supy -. Box.infy))
 
-let draw_state etat = failwith "A DEFINIR"
+let draw_state etat = 
+  draw_briques 
 
 (* extrait le score courant d'un etat : *)
 let score etat : int = failwith "A DEFINIR"
@@ -48,4 +38,4 @@ let draw flux_etat =
   Format.printf "Score final : %d@\n" score;
   Graphics.close_graph ()
 
-let () = game_hello ()
+let ()  = draw 
