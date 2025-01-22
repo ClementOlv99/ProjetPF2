623263e34e25357c6cdef4cfd6a94bf86142128a
Bontinck L. <journaldu21@gmail.com>
2025-01-22 17:34:13 +0100

    Collisions : fonction de collision complète
    luigi

--- source/lib/input.ml
+++ source/lib/input.ml
@@ -8,6 +8,62 @@ let mouse =
       Some ((float_of_int x, Graphics.button_down ()), ()))
     ()
 
+type etat_balle = (float*float)*(float*float)
+type rect = (float*float)*(float*float)
+type ball = (float*float)*float
+
+let is_colliding balle rectangle (vx,vy) : (float*float)  =
+
+  let normalise x y : (float*float) =
+    let abs = x**2.0 +. y**2.0 in
+    (x/.abs),(y/.abs)
+  and vect_to_dir (vx,vy) : (float*float) =
+    if (vx >= vy) then
+      if (vx >= 0.0)
+        then (1.0,0.0)
+        else (-1.0,0.0)
+    else
+      if (vy >= 0.0)
+        then (0.0,1.0)
+        else (0.0,-1.0)
+  in
+  let is_point_in_ball_n px py cx cy r : (float*float) =
+    if (((px -. cx)**2.0) +. (( py -. cy)**2.0 ) <= r**2.0) 
+      then (normalise (cx -. px) (cy -. py))
+      else (0.0,0.0)
+  and is_point_in_rect px py rx ry rtx rty : bool =
+    if ((rx <= px) && (px <= (rx +. rtx)) && (ry <= py) && (py <= (ry +. rty))) then true else false
+  in
+
+  match balle with
+  | (cx,cy),r -> 
+    match rectangle with
+    | (rx,ry),(rtx,rty) ->
+      if (cx < rx) then (* Si à gauche *)
+        if (cy < ry) then (* Si à gauche et en dessous *)
+          (is_point_in_ball_n (rx) (ry) cx cy r)
+        else if (cy > ry +. rty) then (* Si à gauche et au dessus *)
+          (is_point_in_ball_n (rx) (ry +. rty) cx cy r)
+        else (* Si à gauche et dedans (y) *)
+          if (is_point_in_rect (cx+.r) cy rx ry rtx rty)
+            then (-1.0,0.0)
+            else (0.0,0.0)
+      else if (cx > rx +. rtx) then (* Si à droite *)
+        if (cy < ry) then (* Si à droite et en dessous *)
+          (is_point_in_ball_n (rx+.rtx) (ry) cx cy r)
+        else if (cy > ry +. rty) then (* Si à droite et au dessus *)
+          (is_point_in_ball_n (rx +. rtx) (ry +. rty) cx cy r)
+        else (* Si à droite et dedans (y) *)
+          if (is_point_in_rect (cx+.r) cy rx ry rtx rty)
+            then (1.0,0.0)
+            else (0.0,0.0)
+      else (* Si dedans (x) *)
+        if (cy < ry) then (* Si dedans (x) et en dessous *)
+          (0.0,-1.0)
+        else if (cy > ry +. rty) then (* Si dedans (x) et au dessus *)
+          (0.0,1.0)
+        else (* Si dedans (x) et dedans (y) *)
+            (vect_to_dir (vx,vy))
 
 module Collision = struct
 
