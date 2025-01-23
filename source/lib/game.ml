ee45aeb9d58225699266b0ca7469e350585cf678
QuoicouLC <bbalc.olivier@gmail.com>
2025-01-23 10:52:50 +0100

    je fait update balle

--- source/lib/game.ml
+++ source/lib/game.ml
@@ -5,7 +5,11 @@ open Init
 open Dessin
 
 (*etat de la partie*)
-type etat = balle * float * score * (Quadtree.tree * int)
+
+type raquette = (float * float)
+type balle = (float * float) * (float * float)
+type score = (int * int)
+type etat = balle * raquette score * (Quadtree.tree * int)
 
 let game_init liste_brique = 
 
@@ -28,6 +32,28 @@ let raquette_update =
       let dx = (x -. prev_x) /. Init.dt in
       Some ((float_of_int x, dx, Graphics.button_down ()), x))
     ()
+
+let balle_update raquette Flux.t -> palette -> ball -> Quadtree.tree -> ball Flux.t =
+  fun raquette_flux
+    (mouse_x, mouse_dx)
+    ((x, y), (dx, dy))
+    quadtree -> 
+
+    let 
+
+    let ndx = rebond_x br_qtree (x, y) (dx, dy) in
+
+    let ndy = rebond_y br_qtree mouse_x (x, y) (dx, dy) in
+
+    let a_flux = Flux.constant (0.0, -9.81) in
+
+    let v_flux = Flux.map (fun (vx, vy) -> vx +. ndx, vy +. ndy) (integre PhysicsInit.dt a_flux) in
+    let x_flux =
+      Flux.map (fun (nx, ny) -> nx +. x, ny +. y) (integre PhysicsInit.dt v_flux)
+    in
+    let is_launched_flux = Flux.constant new_is_launched in
+
+    Flux.map3 (fun x v b -> x, v, b) x_flux v_flux is_launched_flux
 
 
 
