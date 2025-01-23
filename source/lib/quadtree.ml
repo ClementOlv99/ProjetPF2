2611a4361e1979f5e2e1e334e902d8a390a0c274
Un Certain Jean-Pierre <clement.demazure@etu.inp-n7.fr>
2025-01-23 15:13:59 +0100

    quadtree terminé, rien n'a été testé. Rien du tout EZ

--- source/lib/quadtree.ml
+++ source/lib/quadtree.ml
@@ -1,51 +1,165 @@
-open Init
-
 type coord = float * float
 
 (* coordonnées des coins inférieur gauche et supérieur droit d'une case du quadtree *)
 type limites = coord * coord
 
-
 (** type du quadtree *)
-type tree =
+type quadtree =
   | Leaf of limites * (coord option)
-  | Node of limites * tree * tree * tree * tree
+  | Node of limites * quadtree * quadtree * quadtree * quadtree
+
 
 (******************************************************************************)
-(*      fonction intermediaire de comparaison                                 *)
+(*      fonction interne de calcul de norme                                   *)
 (*   signature :                                                              *)
-(*		inside : (float * float) -> (float * float) * (float * float) -> bool *)
-(*   paramètre(s) : les coordonnées de la balle						          *)
-(*					les coordonnéesd des limites de l'espace				  *)
-(*   résultat     : true si la balle est dans l'espace delimité               *)
-(*			par les coordonnées, false sinon.                                 *)
-(******************************************************************************)
-let inside : coord -> limites -> bool =
-	fun coord_balle intervalle ->
-		let (xb, yb) = coord_balle in
-		let ((xmin , xmax) , (ymin , ymax)) = intervalle in
-		(xb <= xmin) || ((xb >= xmax) || ((yb <= ymin) || (yb >= ymax)))
-
-(******************************************************************************)
-(*      fonction intermediaire de calcul de norme                             *)
-(*   signature :                                                              *)
-(*		norme : (float * float) -> float                                      *)
-(*   paramètre(s) : les coordonnées du vecteur						          *)
-(*   résultat     : la norme du vecteur 	                                  *)
+(*      norme : coord -> float                                                *)
+(*   paramètre(s) : les coordonnées du vecteur                                *)
+(*   résultat     : la norme du vecteur                                       *)
 (******************************************************************************)
 let norme : coord -> float =
-	fun coord_vect ->
-		let (xv, yv) = coord_vect in
-		sqrt ((Float.pow xv 2.0) +. (Float.pow yv 2.0))
+fun coord_vect ->
+	let (xv, yv) = coord_vect in
+	sqrt ((Float.pow xv 2.0) +. (Float.pow yv 2.0))
+
+(******************************************************************************)
+(*      fonction interne pour verifier l'égalité entre 2 points               *)
+(*   signature :                                                              *)
+(*      equal : coord -> coord -> bool                                        *)
+(*   paramètre(s) : les coordonnées des 2 points à comparer                   *)
+(*   résultat     : le booléen resultat                                       *)
+(******************************************************************************)
+let equal : coord -> coord -> bool =
+fun (x_1, y_1) (x_2, y_2) ->
+	(x_1 = x_2) && (y_1 = y_2)
+
+(******************************************************************************)
+(*      batterie de fonctions intenes pour obtenir les limites des            *)
+(*                    sous-quadrants d'un espace                              *)
+(*   signatures :                                                             *)
+(*      quadrant_no : limites -> limites                                      *)
+(*      quadrant_ne : limites -> limites                                      *)
+(*      quadrant_se : limites -> limites                                      *)
+(*      quadrant_so : limites -> limites                                      *)
+(*   paramètre(s) : les limites du sous espace à découper                     *)
+(*   résultat     : les limites du quadrant resultant                         *)
+(******************************************************************************)
+let quadrant_no : limites -> limites =
+fun ((x_so, y_so),(x_ne, y_ne)) ->
+	((x_so, (y_ne +. y_so) /. 2.0), ((x_ne +. x_so) /. 2.0, y_ne))
+let quadrant_ne : limites -> limites =
+fun ((x_so, y_so),(x_ne, y_ne)) ->
+	(((x_ne +. x_so) /. 2.0, (y_ne +. y_so) /. 2.0), (x_ne, y_ne))
+let quadrant_se : limites -> limites =
+fun ((x_so, y_so),(x_ne, y_ne)) ->
+	((x_so, (y_ne +. y_so) /. 2.0), ((x_ne +. x_so) /. 2.0, y_ne))
+let quadrant_so : limites -> limites =
+fun ((x_so, y_so),(x_ne, y_ne)) ->
+	((x_so, y_so) , ((x_ne +. x_so) /. 2.0, (y_ne +. y_so) /. 2.0))
+
+(******************************************************************************)
+(*      fonction interne pour placer objet dans espace (quel quart?)          *)
+(*   signature :                                                              *)
+(*      placement : coord -> limites -> bool                                  *)
+(*   paramètre(s) : les coordonnées de l'objet à étudier                      *)
+(*                  les limites de l'espace à tester                          *)
+(*   résultat     : un couple de booléen indiquant le quadrant dans lequel    *)
+(*                  l'objet doit être placé:                                  *)
+(*      |true,true   -> quadrant nord-ouest                                   *)
+(*      |true,false  -> quadrant nord-est                                     *)
+(*      |false,false -> quadrant sud-est                                      *)
+(*      |false,true  -> quadrant sud-ouest                                    *)
+(******************************************************************************)
+let placement : coord -> limites -> (bool * bool) =
+fun (x_obj, y_obj) ((x_so, y_so), (x_ne, y_ne)) ->
+	(x_obj > ((x_so +. x_ne) /. 2.0), y_obj > ((y_so +. y_ne) /. 2.0))
 
 
-let predict : int -> coord -> (float * float) -> coord list =
-	fun r coord_balle vitesse_balle ->
-		let (xb, yb) = coord_balle in
-		let (vx, vy) = vitesse_balle in
-		let (dir_x, dir_y) = ((vx /. norme(vitesse_balle)), (vy /. norme(vitesse_balle))) in
-		let (pred_x, pred_y) = (xb +. (r *. dir_x)) ,(yb +. (r *. dir_y)) in
-		let a = r dir_x in a
+let predict : float -> ((float * float) * (float * float)) -> (float*float) list =
+fun r ((xb, yb), vitesse_balle) ->
+	let (vx, vy) = vitesse_balle in
+	let (dir_x, dir_y) = ((vx /. norme(vitesse_balle)), (vy /. norme(vitesse_balle))) in
+	let (pred_x, pred_y) = (xb +. (r *. dir_x)) ,(yb +. (r *. dir_y)) in
+		(pred_x +. (dir_x*.r) , pred_y +. (dir_y*.r)) ::((pred_x +. (dir_y*.r) , pred_y -. (dir_x*.r)) :: ((pred_x -. (dir_y*.r) , pred_y +. (dir_x*.r))::[]))
 
+let rec find_tree: quadtree -> coord -> coord option =
+fun tree coord_balle ->
+	match tree with
+		|Leaf (_, None) -> None
+		|Leaf (_, brick) -> brick
+		|Node (limites, br_no, br_ne, br_se, br_so) ->
+			match placement coord_balle limites with
+				|true,true   -> find_tree br_no coord_balle
+				|true,false  -> find_tree br_ne coord_balle
+				|false,true  -> find_tree br_so coord_balle
+				|false,false -> find_tree br_se coord_balle
 
-		(*x + "r*(vx/norme(v))" + vx*)
+let rec insert_tree : quadtree -> coord -> quadtree =
+fun tree coord_new_br ->
+	match tree with
+		|Leaf (lim, None)  -> Leaf (lim, Some(coord_new_br))
+		|Leaf (lim, Some(coord_old_br)) ->
+			let new_node = Node(lim, Leaf(quadrant_no lim, None), Leaf(quadrant_ne lim, None), Leaf(quadrant_se lim, None), Leaf(quadrant_so lim, None)) in
+				insert_tree (insert_tree new_node coord_old_br) coord_new_br
+		|Node (lim, br_no, br_ne, br_se, br_so) ->
+			match placement coord_new_br lim with
+				|true,true   -> insert_tree br_no coord_new_br
+				|true,false  -> insert_tree br_ne coord_new_br
+				|false,true  -> insert_tree br_so coord_new_br
+				|false,false -> insert_tree br_se coord_new_br
+
+let create_tree : limites -> coord list -> quadtree =
+fun lim briques ->
+	let rec aux : quadtree -> coord list -> quadtree =
+	fun tree briques -> match briques with
+		|[] -> tree
+		|(t::q) -> aux (insert_tree tree t) q
+		in aux (Leaf(lim, None)) briques
+
+(******************************************************************************)
+(*      fonction interne de nettoyage du quadtree pour eviter les             *)
+(*                       embranchement inutiles.                              *)
+(*   signature :                                                              *)
+(*      clean : quadtree -> quadtree                                          *)
+(*   paramètre(s) : le quadtree à purifier                                    *)
+(*   résultat     : le quadtree post opération, plus propre.                  *)
+(******************************************************************************)
+let rec clean : quadtree -> quadtree =
+fun tree ->
+	let clean_etage : quadtree -> quadtree =
+	fun tree -> match tree with
+		|Node (lim, Leaf(_, None), Leaf(_, None), Leaf(_, None), Leaf(_, None)) -> Leaf(lim, None)
+		|Node (lim, Leaf(_, Some(coord)), Leaf(_, None), Leaf(_, None), Leaf(_, None))  -> Leaf(lim, Some(coord))
+		|Node (lim, Leaf(_, None), Leaf(_, Some(coord)), Leaf(_, None), Leaf(_, None))  -> Leaf(lim, Some(coord))
+		|Node (lim, Leaf(_, None), Leaf(_, None), Leaf(_, Some(coord)), Leaf(_, None)) -> Leaf(lim, Some(coord))
+		|Node (lim, Leaf(_, None), Leaf(_, None), Leaf(_, None), Leaf(_, Some(coord)))  -> Leaf(lim, Some(coord))
+		|_ -> tree
+		in
+	match tree with
+		|Leaf (_, _) -> tree
+		|Node (lim, br_no, br_ne, br_se, br_so) -> Node(lim, clean_etage br_no, clean_etage br_ne, clean_etage br_se, clean_etage br_so)
+
+(******************************************************************************)
+(*      fonction interne qui retire une brique d'un quadtree.                 *)
+(*   signature :                                                              *)
+(*      kill : quadtree -> coord -> quadtree                                  *)
+(*   paramètre(s) : le quadtree d'où l'on veut retirer la brique.             *)
+(*                  les coordonnées de la brique à retirer.                   *)
+(*   résultat     : le quadtree post opération, plus propre.                  *)
+(******************************************************************************)
+let rec kill : quadtree -> coord -> quadtree =
+fun tree coord_cible ->
+	match tree with
+		|Leaf(_, None) -> tree
+		|Leaf(lim, Some(coord)) -> if (equal coord coord_cible) then Leaf(lim, None) else tree
+		|Node (lim, br_no, br_ne, br_se, br_so) ->
+			match placement coord_cible lim with
+				|true,true   -> kill br_no coord_cible
+				|true,false  -> kill br_ne coord_cible
+				|false,true  -> kill br_so coord_cible
+				|false,false -> kill br_se coord_cible
+
+let rec purge_tree : quadtree -> coord list -> quadtree =
+fun tree briques ->
+	match briques with
+		|[]     -> clean tree
+		|(t::q) -> purge_tree (kill tree t) q
--- source/lib/quadtree.mli
+++ source/lib/quadtree.mli
@@ -1,65 +1,91 @@
-open Init
-
-
 (*                          Module QuadTree                                   *)
 
-type tree
+type quadtree
+(**
+	le type du QuadTree à proprement parler.
+*)
+
 type coord
+(**
+	pour stocker les coordonnées de différents objets: coord = (float * float) .
+*)
 type limites
+(**
+	pour delimiter un espace, on stock le coin inferieur gauche (noté dans le code "sud-est") et le coin superieur droit (noté dans le code "nord-ouest"): limites = (coord * coord) .
+*)
+type balle
+(**
+	À retirer car deja mis dans game, juste pour eviter les erreurs d'import pendant la redéction du code.
+*)
 
-(******************************************************************************)
-(*                                                                            *)
-(*      fonction de calcul des points de la prédiction de la balle            *)
-(*                                                                            *)
-(*   signature :                                                              *)
-(*		predict : int -> (int*int) -> (float * float) -> coord list  	 	  *)
-(*                                                                            *)
-(*   paramètre(s) : le rayon de la balle									  *)
-(*					les coordonnées de la balle								  *)
-(*					les vitesses de la balle								  *)
-(*   résultat     : les trois points de test de colisions future		      *)
-(*                                                                            *)
-(******************************************************************************)
-val predict : int -> balle -> coord list
+val predict : float -> ((float * float) * (float * float)) -> coord list
+(**
+	fonction de calcul des points de la prédiction de la balle.
+	- signature : predict : float -> ((float * float) * (float * float)) -> coord list
 
+	- paramètre(s) :
+		- le rayon de la balle.
+		- (les coordonnées de la balle * les vitesses de la balle).
 
-(******************************************************************************)
-(*                                                                            *)
-(*      fonction de création du quadtree initial				              *)
-(*                                                                            *)
-(*   signature : create_tree : (int * int) list -> quadtree                   *)
-(*                                                                            *)
-(*   paramètre(s) : les limites de l'écran									  *)
-(*					la liste des briques                                      *)
-(*   résultat     : un quadtree avce la position de chacunes des briques      *)
-(*                                                                            *)
-(******************************************************************************)
-val create_tree : limites -> coord list -> tree
+	- résultat :
+		- les trois points de test de colisions future.
+*)
 
-(******************************************************************************)
-(*                                                                            *)
-(*      fonction de recherche de brique à portée 					 	      *)
-(*                                                                            *)
-(*   signature : find_brique : quadtree -> coord -> coord option  *)
-(*                                                                            *)
-(*   paramètre(s) : le quadtree representant le niveau						  *)
-(*					un couple de float representant la position de la balle   *)
-(*   résultat     : None si pas de briques à portée							  *)
-(*  	Some (coord) les coordonnées de la brique incriminée sinon	      *)
-(*                                                                            *)
-(******************************************************************************)
-val find_brique : tree -> coord -> coord option
+val create_tree : limites -> coord list -> quadtree
+(**
+	fonction de création du QuadTree initial.
+	- signature : create_tree : coord list -> tree
 
-(******************************************************************************)
-(*                                                                            *)
-(*      fonction de retrait d'une brique d'un quadtree                        *)
-(*                                                                            *)
-(*   signature : destroy : quadtree -> (int * int) -> quadtree                *)
-(*                                                                            *)
-(*   paramètre(s) : le quadtree representant le niveau						  *)
-(*					un couple d'entiers representant la position de la brique *)
-(*   résultat     : le quadtree corrigé                                       *)
-(*                                                                            *)
-(******************************************************************************)
-val destroy : tree -> coord -> tree
+	- paramètre(s) :
+		- les limites de l'écran.
+		- la liste des briques.
+
+	- résultat :
+		- un QuadTree avec la position de chacunes des briques.
+*)
+
+val insert_tree : quadtree -> coord -> quadtree
+(**
+	fonction pour rajouter une brique dans un QuadTree en respectant la structure du QuadTree et ses invariants.
+	- insert_tree : tree -> coord -> tree
+
+	- paramètre(s) :
+		- le QuadTree incomplet.
+		- les coordonnées de la brique à rajouter.
+
+	- résultat :
+		- le QuadTree avec la brique en plus.
+
+	- cas d'erreurs:
+		- "Rajouter une brique déjà dans presente dans le QuadTree" mène à une boucle infini et c'est relou.
+*)
+
+val find_tree : quadtree -> coord -> coord option
+(**
+	fonction de recherche de brique à portée.
+
+	- signature : find_tree : tree -> coord -> coord option
+
+	- paramètre(s) :
+		- le QuadTree representant le niveau.
+		- un couple de float representant la position de la balle.
+
+	- résultat :
+		- None si pas de briques à portée.
+		- Some (coord) les coordonnées de la brique incriminée sinon.
+*)
+
+val purge_tree : quadtree -> coord list -> quadtree
+(**
+	fonction de retrait d'une liste de briques d'un QuadTree.
+
+	- signature : purge_tree : quadtree -> coord -> quadtree
+
+	- paramètre(s) :
+		- le QuadTree representant le niveau.
+		- une liste de couple d'entiers representant les positions des briques à retirer.
+
+	- résultat :
+		- le QuadTree corrigé.
+*)
 
