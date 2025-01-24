83321151182f6587b7b8e4719e81ae519014e859
QuoicouLC <bbalc.olivier@gmail.com>
2025-01-24 14:15:09 +0100

    ça compile

--- source/lib/game.ml
+++ source/lib/game.ml
@@ -2,7 +2,6 @@ open Quadtree
 open Iterator
 open Input
 open Init
-open Dessin
 
 (*etat de la partie*)
 
@@ -15,7 +14,9 @@ let game_init liste_brique =
 
   let balle = ((Box.supx/.2., Box.supy/.2.), (0., 0.)) in
 
-  let raquette = mouse in
+  let x, _ = Graphics.mouse_pos () in
+
+  let raquette = (x, 0) in  
 
   let score = 0, 3 in
 
@@ -30,12 +31,11 @@ let raquette_update =
     (fun prev_x ->
       let x, _ = Graphics.mouse_pos () in
       let dx = ((float_of_int x) -. prev_x) /. Data.dt in
-      Some ((float_of_int x, dx, Graphics.button_down ()), float_of_int x))
+      Some ((float_of_int x, dx), float_of_int x))
       0.0
 
-let balle_update : raquette Flux.t -> raquette -> balle -> quadtree -> balle Flux.t =
-  fun raquette_flux
-    (mouse_x, mouse_dx)
+let balle_update : raquette -> balle -> quadtree -> balle Flux.t =
+  fun (mouse_x, mouse_dx)
     ((x, y), (dx, dy))
     quadtreeB -> 
 
@@ -60,13 +60,13 @@ let balle_update : raquette Flux.t -> raquette -> balle -> quadtree -> balle Flu
       fun ((x,y), (dx, dy)) ->
         
         let a = Flux.constant (0.0, -.g) in
-        let v = Flux.map (fun (vx, vy) -> (vx +. dx, vy +. dy)) (Collision.integre Data.dt a) in
-        let p = Flux.map (fun (px, py) -> (px +. x, py +. y)) (Collision.integre Data.dt v) in
+        let v = Flux.map (fun (vx, vy) -> (vx +. dx, vy +. dy)) (integre Data.dt a) in
+        let p = Flux.map (fun (px, py) -> (px +. x, py +. y)) (integre Data.dt v) in
 
         Flux.unless (Flux.unless (Flux.map2 (fun pn vn -> (pn, vn)) p v) (fun ((x,y),(dx,dy)) -> (collision (x,y) (dx,dy) = 1) || Collision.contact_x x dx || Collision.contact_y y dy) (fun ((x,y), (dx, dy)) -> run_collision ((x, y), (Collision.rebond_x x dx, Collision.rebond_y y dy)))) (fun ((x,y),(dx,dy)) -> collision (x,y) (dx,dy) = 2) (fun ((x,y),(dx,dy)) -> run ((x,y), (-.dx, -.dy)))
 
         
-      and run_collision : balle -> balle Flux.t =
+    and run_collision : balle -> balle Flux.t =
       fun ((x,y), (dx, dy)) ->
 
         let a = Flux.constant (0.0, -.g) in
@@ -77,7 +77,7 @@ let balle_update : raquette Flux.t -> raquette -> balle -> quadtree -> balle Flu
           | (0.0,0.0) -> run ((x,y), (dx, dy))
       in
 
-      run ((x,y), (dx, dy))
+    run ((x,y), (dx, dy))
 
     
           
@@ -85,22 +85,50 @@ let balle_update : raquette Flux.t -> raquette -> balle -> quadtree -> balle Flu
 
 
 
-let score_update : score -> balle -> score Flux.t = 
+let score_update : score -> balle -> (score Flux.t * int) = 
   fun (current_score, lives) ((x, y), (_, _)) ->
     if y < 0. then
-      Flux.constant (current_score, lives - 1)
+      if (lives - 1) = 0 then
+        (Flux.constant (current_score, lives - 1), 0)
+      else
+        (Flux.constant (current_score, lives - 1), 1)
+
     else
-      Flux.constant (current_score, lives)
+        (Flux.constant (current_score, lives), 1)
 
 
-(*let game_update : etat -> etat Flux.t =
+let game_update : etat -> etat Flux.t =
   fun (balle, raquette, score, (quadtreeB, nbBrique)) ->
-    let raquette_flux = raquette_update in
-    let balle_flux = balle_update raquette_flux raquette balle quadtreeB in
-    let score_flux = score_update score nbBrique balle in
+    
+    let balle_flux = balle_update  raquette balle quadtreeB in
+    
+    let (x,y), (dx,dy) = balle in 
 
-    let (x,y), (dx,dy) = balle in
-*)
+    (*let briques = find_briques quadtreeB (x,y) (dx,dy) in*)
+    let nbBrique = 0 in
+    (*to do: 
+     - Détruire brique
+    *) 
+
+    let nbBrique = 0 in
+
+    let raquette_flux = raquette_update in
+
+    let (score_flux, lives) = score_update score balle in
+
+
+    if nbBrique = 0 || lives = 0  then
+      Flux.constant (balle, raquette, score, (quadtreeB, nbBrique))
+    else
+      let map4 f i1 i2 i3 i4 = Flux.(apply (apply (apply (apply (constant f) i1) i2) i3) i4) in
+
+      map4 (fun b r s q -> (b, r, s, q)) balle_flux raquette_flux score_flux (Flux.constant (quadtreeB, nbBrique))
+
+    
+
+      
+
+
     
 
 
